36615dab2167d6c3654de860f3f3e382a44c1258
pocketchan <81311319+pocketchan@users.noreply.github.com>
2023-12-23 14:30:39 +0900

    able to calc and show in the terminal

--- src/bin2dec/src/main.rs
+++ src/bin2dec/src/main.rs
@@ -1,58 +1,92 @@
+use iced::widget::text_input;
+use iced::Sandbox;
+
 fn main() {
-    let mut settings = Settings::default();
-    settings.window.size = (360u32, 240u32);
-    let _ = Counter::run(settings);
+    // let mut settings = Settings::default();
+    // settings.window.size = (360u32, 240u32);
+    // let _ = ConverterBinToDec::run(settings);
+    
+    // can calc decimal
+    // but it is hard to use iced
+    // so stop in here. I will use tauri.
+    // not need to use many types of gui, I'm beginner still.
+    let mut converter: ConverterBinToDec = ConverterBinToDec::new(101);
+    let sum = converter.converted();
+    println!("{}", sum);
 }
 
-struct Counter {
-    // The counter value
-    value: i32,
+pub struct ConverterBinToDec {
+    input_value: String,
+    binary: u32,
+    converted: u32,
+}
+
+impl ConverterBinToDec {
+    pub fn new(binary_input: u32) -> ConverterBinToDec {
+        ConverterBinToDec {
+            input_value: String::new(),
+            binary: binary_input,
+            converted: 0,
+        }
+    }
+
+    pub fn converted(&mut self) -> u32 {
+        let binary_str = self.binary.to_string();
+        let mut added: u32 = 0;
+        for digit in (0..binary_str.len()).rev() {
+            let digit_value = &binary_str[digit..digit + 1];
+            added += digit_value.parse::<u32>().unwrap() * 2_u32.pow(digit as u32);
+            println!();
+        }
+
+        added
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
-    IncrementPressed,
-    DecrementPressed,
+    ConvertedPressed,
+    InputChanged,
 }
 
-use iced::{widget::{button, column, text, Button, Text}
-    , Sandbox, Element, Alignment, Settings, alignment::Horizontal, Length};
+use iced::{widget::{button, column, text, Button}
+    , Element, Alignment, alignment::Horizontal, Length};
 
-impl Sandbox for Counter {
+impl Sandbox for ConverterBinToDec {
     type Message = Message;
 
     fn new() -> Self {
-        Self { value: 0 }
+        Self {
+            input_value: String::new(),
+            binary: 0,
+            converted: 0,
+        }
     }
 
     fn title(&self) -> String {
-        String::from("Counter - Iced")
+        String::from("Bin to Dec Converter")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Self::Message) {
         match message {
-            Message::IncrementPressed => {
-                self.value += 1;
+            Message::ConvertedPressed => {
+                self.converted = self.converted();
             }
-            Message::DecrementPressed => {
-                self.value -= 1;
-            }
+            Message::InputChanged => todo!(),
         }
     }
 
     fn view(&self) -> Element<Message> {
         // We use a column: a simple vertical layout
         column![
-            // The increment button. We tell it to produce an
-            // `IncrementPressed` message when pressed
-            Button::new("+").on_press(Message::IncrementPressed),
+            text(self.converted)
+                .size(50).horizontal_alignment(Horizontal::Center),
 
-            // We show the value of the counter here
-            text(self.value).size(50).horizontal_alignment(Horizontal::Center),
+            text_input("enter value", &self.input_value)
+                .on_submit(Message::InputChanged)
+                .size(50),
 
-            // The decrement button. We tell it to produce a
-            // `DecrementPressed` message when pressed
-            button("-").on_press(Message::DecrementPressed),
+            button("Convert!").on_press(Message::ConvertedPressed),
         ]
         .width(Length::Fill)
         .height(Length::Fill)
